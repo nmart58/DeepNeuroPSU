da7b719a99b59f83b3c684f965234a5aa68f60c6
nmart58 <91692987+nmart58@users.noreply.github.com>
2025-12-12 00:03:07 +0300

    Add files via upload

---
+++ Lab7_yolo_detector (1).py
@@ -0,0 +1,20 @@
+﻿from ultralytics import YOLO
+import matplotlib as plt
+
+# Загрузка предобученной модели YOLOv5
+model = YOLO("yolov8s.pt")
+
+# Путь к YAML файлу, который содержит данные для обучения
+data_yaml = "data1.yaml"
+
+# Обучение модели
+model.train(data=data_yaml, epochs=25, batch=16, imgsz=640)
+
+# Сохранение обученной модели
+model.save("trained_model.pt")
+
+# Тестирование модели на новом изображении
+results = model("image_result.jpg")
+
+# Показ результатов
+results[0].show()
